44926f78efbc1b9c5adc6d25bcc12c6a44b69a6f
Hajar ALfarsi <hajar2000alfarsi@gamil.com>
2026-04-07 11:10:08 +0400

    answer of Task4

---
+++ src/task4.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class task4 {
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter a number ");
+        int num=sc.nextInt();
+        task4 ex4=new task4();
+        System.out.println(ex4.more20(num));
+    }
+    public boolean more20(int n){
+        boolean result=false;
+        if(n%20==1 || n%20==2){
+            result=true;
+        }
+        else{
+            result=false;
+        }
+        return result;
+    }
+}
